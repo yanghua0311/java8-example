24213d23678bdd6ebd7ea10a1c11dcc3cb6a93c0
yanghua <879651110@qq.com>
2017-12-11 20:39:52 +0800

    format code

--- src/com/young/java8/DemoLambda.java
+++ src/com/young/java8/DemoLambda.java
@@ -6,40 +6,44 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
-public class DemoLambda  {
-	
-	static void test(){
-		//输出一个参数
-		Supplier<String> sup=()->"youngwa";
+public class DemoLambda {
+
+	static void test() {
+		// 输出一个参数
+		Supplier<String> sup = () -> "youngwa";
 		System.out.println(sup.get());
-		//消费一个输入参数
-		Consumer<String> consumer=str->{System.out.println(str);};
+
+		// 消费一个输入参数
+		Consumer<String> consumer = str -> {
+			System.out.println(str);
+		};
 		consumer.accept("youngwa");
-		//消费两个输入参数
-		BiConsumer<Integer,String> bicon=(in,str)->{
-			System.out.println(in.toString()+str);
+
+		// 消费两个输入参数
+		BiConsumer<Integer, String> bicon = (in, str) -> {
+			System.out.println(in.toString() + str);
 		};
 		bicon.accept(21, "young");
-		//一个输入参数，一个输出参数
-		Function<Integer,Integer> fun=a->{
+
+		// 一个输入参数，一个输出参数
+		Function<Integer, Integer> fun = a -> {
 			int sum = 0;
-			for(int i=1;i<=a;i++) {
+			for (int i = 1; i <= a; i++) {
 				sum += i;
 			}
 			return sum;
-		}; 
-		System.out.println(fun.apply(10));
-		//接受两个参数，一个输入参数，可以实现生成一些转型生成code，
-		BiFunction<Integer,Long,String> parse=(id,code)->{
-			return id.toString()+"和"+code.toString();
 		};
-		String co=parse.apply(10, 200L);
+		System.out.println(fun.apply(10));
+
+		// 接受两个参数，一个输入参数，可以实现生成一些转型生成code，
+		BiFunction<Integer, Long, String> parse = (id, code) -> {
+			return id.toString() + "和" + code.toString();
+		};
+		String co = parse.apply(10, 200L);
 		System.out.println(co);
 	}
-	
+
 	public static void main(String[] args) {
 		test();
 	}
 }
-
-
--- src/com/young/java8/DemoMethodRefere.java
+++ src/com/young/java8/DemoMethodRefere.java
@@ -3,21 +3,25 @@ package com.young.java8;
 import java.util.function.Supplier;
 
 public class DemoMethodRefere {
-	static String get(){
+	static String get() {
 		return "young";
 	}
-	 String get1(){
-			return "young";
-		}
-	static void test(){
-		DemoMethodRefere de=new DemoMethodRefere();
-		Supplier<String> sup=de::get1;
+
+	String getForNew() {
+		return "young";
+	}
+
+	static void test() {
+		DemoMethodRefere de = new DemoMethodRefere();
+		Supplier<String> sup = de::getForNew;  				//实例方法引用
+	//  Supplier<String> sup = DemoMethodRefere::get;   	//静态方法引用
 		System.out.println(sup.get());
-		FunctionInDEmo fun=(str,dou)->{
-			return Integer.valueOf(str)+dou.intValue();
+		FunctionInDEmo fun = (str, dou) -> {
+			return Integer.valueOf(str) + dou.intValue(); 	//自定义函数式接口
 		};
 		System.out.println(fun.toInt("1111", 66.99));
 	}
+
 	public static void main(String[] args) {
 		test();
 	}
