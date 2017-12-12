1835b7ba274a1d67d88ba76e8e7b434dfed0b411
yanghua <879651110@qq.com>
2017-12-12 19:51:44 +0800

    format code

--- src/com/young/java8/DemoMethodRefere.java
+++ src/com/young/java8/DemoMethodRefere.java
@@ -13,11 +13,11 @@ public class DemoMethodRefere {
 
 	static void test() {
 		DemoMethodRefere de = new DemoMethodRefere();
-		Supplier<String> sup = de::getForNew;  				//实例方法引用
-	//  Supplier<String> sup = DemoMethodRefere::get;   	//静态方法引用
+		Supplier<String> sup = de::getForNew; // 实例方法引用
+		// Supplier<String> sup = DemoMethodRefere::get; //静态方法引用
 		System.out.println(sup.get());
 		FunctionInDEmo fun = (str, dou) -> {
-			return Integer.valueOf(str) + dou.intValue(); 	//自定义函数式接口
+			return Integer.valueOf(str) + dou.intValue(); // 自定义函数式接口
 		};
 		System.out.println(fun.toInt("1111", 66.99));
 	}
--- src/com/young/java8/VaadinProviderDemo.java
+++ src/com/young/java8/VaadinProviderDemo.java
@@ -3,12 +3,12 @@ package com.young.java8;
 import java.util.function.Function;
 
 public interface VaadinProviderDemo<T, R> extends Function<T, R> {
-	
-	 public static <T> VaadinProviderDemo<T, T> identity() {
-	        return t -> t;
-	    }
 
-	 @Override
-	    public R apply(T source);
-	 
+	public static <T> VaadinProviderDemo<T, T> identity() {
+		return t -> t;
+	}
+
+	@Override
+	public R apply(T source);
+
 }
